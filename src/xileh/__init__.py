7d913725ed33a2d5fe12e5876a122966df83d604
matthiasdold <matthias.dold@gmx.net>
2021-06-21 17:25:10 +0200

    Made importing of xPipeline and xPdata shorter

--- src/xileh/__init__.py
+++ src/xileh/__init__.py
@@ -2,3 +2,8 @@
 # ie. the setup.cfg
 import importlib.metadata
 __version__ = importlib.metadata.version('xileh')
+
+
+# get the two main components for easier import
+from xileh.core.pipeline import xPipeline
+from xileh.core.pipelinedata import xPData
